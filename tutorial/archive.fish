7a052d0e9cff49ceeb5f838e1a0cd942ac995929
shinyay <shinya.com@gmail.com>
2021-09-25 09:09:34 +0900

    Add tutorial lab registry url

--- tutorial/archive.fish
+++ tutorial/archive.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
-git clone -b 1.0.0 https://github.com/shinyay/codelab-spring-boot-rest-code.git tutorial
+git clone -b 1.0.0 git@github.com:shinyay/codelab-spring-cloud-gcp-mysql-code-java.git tutorial
 rm -fr tutorial/.git
 tar cvfz tutorial.tar.gz tutorial
 rm -fr tutorial
